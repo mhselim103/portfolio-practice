7eec3db7f44792dad2081feacc82ec1218f74c0c
Mahmudul Hasan Selim <mhselim103@gmail.com>
2022-04-22 22:23:38 +0600

    navbar

---
+++ src/Pages/Navbar/Navbar.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="n-wrapper">
+      <div className="n-left">
+        <div className="n-name">Selim</div>
+        <span>toggle</span>
+      </div>
+      <div className="n-right">
+        <div className="n-list">
+          <ul>
+            <li>Home</li>
+            <li>Services</li>
+            <li>Experience</li>
+            <li>Portfolio</li>
+            <li>testimonial</li>
+          </ul>
+        </div>
+        <button className="button n-button">Contact Us</button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
