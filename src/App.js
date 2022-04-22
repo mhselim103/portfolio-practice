4d106ce63355fafa1fc3648898faa1a3c64738df
Mahmudul Hasan Selim <mhselim103@gmail.com>
2022-04-23 02:35:16 +0600

    experience

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Experience from "./Pages/Experience/Experience";
 import Intro from "./Pages/Intro/Intro";
 import Navbar from "./Pages/Navbar/Navbar";
 import Services from "./Pages/Services/Services";
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Intro />
       <Services />
+      <Experience />
     </div>
   );
 }
